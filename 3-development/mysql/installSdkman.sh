38b4b9f20aa98cf47b25cea1c1ec41e68404e4ee
Mikhail Asadchy <mikhail.asadchy@idt.net>
2021-03-18 19:09:39 +0300

    add sdkman install script

---
+++ 3-development/mysql/installSdkman.sh
@@ -0,0 +1,9 @@
+# https://sdkman.io/
+curl -s "https://get.sdkman.io" | bash
+
+# https://www.baeldung.com/java-sdkman-intro
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk version
+
+sdk list java
